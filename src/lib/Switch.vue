8da960b0c0a49febd55cd75bc7b25101a0715031
Lemon <516330797@qq.com>
2021-02-02 17:22:50 +0800

    动画流畅

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -35,6 +35,7 @@
     width: $h2;
     background: white;
     border-radius: $h2 / 2;
+    transition: left 250ms;
   }
 
   button.checked {
@@ -43,5 +44,9 @@
 
   button.checked > span {
     left: calc(100% - #{$h2} - 2px);
+  }
+
+  button:focus {
+    outline: none;
   }
 </style>
