1e466e8f383564ae2046d40e6f7a202909724aed
Lemon <516330797@qq.com>
2021-02-03 07:31:42 +0800

    创建 button 组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-    <Button>你好</Button>
+    <Button @click="onClick" @focus="onClick" @mouseover="onClick">你好</Button>
   </div>
 </template>
 <script lang="ts">
   import Button from '../lib/Button.vue';
 
   export default {
-    components: {Button}
+    components: {Button},
+    setup() {
+      const onClick = () => {
+        console.log('hi');
+      };
+      return {onClick};
+    }
   };
 </script>
