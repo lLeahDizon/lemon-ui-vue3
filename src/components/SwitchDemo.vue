a7f3f681786f2d1e1dc77efd7ef86666a497f7fe
Lemon <516330797@qq.com>
2021-02-03 06:29:33 +0800

    优化代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Switch v-model:value="y"/>
+    <Switch v-model:value="bool"/>
   </div>
 </template>
 <script lang="ts">
@@ -10,8 +10,8 @@
   export default {
     components: {Switch},
     setup() {
-      const y = ref(false);
-      return {y};
+      const bool = ref(false);
+      return {bool};
     }
   };
 </script>
