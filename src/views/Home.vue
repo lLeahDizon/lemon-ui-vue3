a1c91a7559bed9ac5ec10fba5bf6b1fecc0fb6ab
Lemon <516330797@qq.com>
2021-02-02 09:52:40 +0800

    topnav 样式更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <div class="topnav">
-      <div class="logo"></div>
-      <div class="menu"></div>
+      <div class="logo">LOGO</div>
+      <ul class="menu">
+        <li>菜单1</li>
+        <li>菜单2</li>
+      </ul>
     </div>
     <div class="banner">
       <h1>Lemon UI</h1>
@@ -17,6 +20,23 @@
 
 <style lang="scss" scoped>
   .topnav {
-    border: 1px solid red;
+    background: pink;
+    display: flex;
+    padding: 16px;
+
+    > .logo {
+      max-width: 6em;
+      margin-right: auto;
+    }
+
+    > .menu {
+      display: flex;
+      white-space: nowrap;
+      flex-wrap: nowrap;
+
+      > li {
+        margin: 0 1em;
+      }
+    }
   }
 </style>
