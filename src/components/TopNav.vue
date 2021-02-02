797ca5a63ccaefa5b87c09a2ee2a402ee1550f4e
Lemon <516330797@qq.com>
2021-02-02 11:56:00 +0800

    添加 toggleAside

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -5,7 +5,7 @@
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
-    <span class="toggleMenu"></span>
+    <span class="toggleAside"></span>
   </div>
 </template>
 
@@ -48,7 +48,15 @@
       }
     }
 
-    > .toggleMenu {
+    > .toggleAside {
+      display: inline-block;
+      width: 24px;
+      height: 24px;
+      background: red;
+      position: absolute;
+      left: 16px;
+      top: 50%;
+      transform: translateY(-50%);
     }
 
     @media (max-width: 500px) {
