ec1d42f54bd922d715c8526aaed9278e02f9addd
Lemon <516330797@qq.com>
2021-02-04 07:35:19 +0800

    支持 level 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -17,13 +17,18 @@
         type: String,
         default: 'normal'
       },
+      level: {
+        type: String,
+        default: 'normal',
+      },
     },
     setup(props) {
-      const {theme, size} = props;
+      const {theme, size, level} = props;
       const classes = computed(() => {
         return {
           [`lemon-theme-${theme}`]: theme,
           [`lemon-size-${size}`]: size,
+          [`lemon-level-${level}`]: level,
         };
       });
       return {classes};
@@ -37,6 +42,7 @@
   $color: #333;
   $blue: #40a9ff;
   $radius: 4px;
+  $red: red;
   .lemon-button {
     box-sizing: border-box;
     height: $h;
@@ -51,6 +57,7 @@
     border: 1px solid $border-color;
     border-radius: $radius;
     box-shadow: 0 1px 0 fade-out(black, 0.95);
+    transition: background 250ms;
 
     & + & {
       margin-left: 8px;
@@ -102,6 +109,63 @@
       font-size: 12px;
       height: 20px;
       padding: 0 4px;
+    }
+
+    &.lemon-theme-button {
+      &.lemon-level-main {
+        background: $blue;
+        color: white;
+        border-color: $blue;
+
+        &:hover,
+        &:focus {
+          background: darken($blue, 10%);
+          border-color: darken($blue, 10%);
+        }
+      }
+
+      &.lemon-level-danger {
+        background: $red;
+        border-color: $red;
+        color: white;
+
+        &:hover,
+        &:focus {
+          background: darken($red, 10%);
+          border-color: darken($red, 10%);
+        }
+      }
+    }
+
+    &.lemon-theme-link {
+      &.lemon-level-danger {
+        color: $red;
+
+        &:hover,
+        &:focus {
+          color: darken($red, 10%);
+        }
+      }
+    }
+
+    &.lemon-theme-text {
+      &.lemon-level-main {
+        color: $blue;
+
+        &:hover,
+        &:focus {
+          color: darken($blue, 10%);
+        }
+      }
+
+      &.lemon-level-danger {
+        color: $red;
+
+        &:hover,
+        &:focus {
+          color: darken($red, 10%);
+        }
+      }
     }
   }
 </style>
