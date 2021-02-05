4744bd279e548dfd9b6b28a76b7ab9f8b2cbb9f2
Lemon <516330797@qq.com>
2021-02-05 15:36:11 +0800

    component :is="current" :key="selected" 需要通过更新 key 来实现动态更新 component

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,19 +1,24 @@
 <template>
   <div>Tabs 示例</div>
   <h1>示例1</h1>
-  <Tabs>
-    <Tab title="导航1">内容1</Tab>
-    <Tab title="导航2">内容2</Tab>
+  <Tabs v-model:selected="x">
+    <Tab key="1" title="导航1">内容1</Tab>
+    <Tab key="2" title="导航2">内容2</Tab>
   </Tabs>
 </template>
 <script lang="ts">
   import Tabs from '../lib/Tabs.vue';
   import Tab from '../lib/Tab.vue';
+  import {ref} from 'vue';
 
   export default {
     components: {
       Tabs,
       Tab,
     },
+    setup() {
+      const x = ref('1');
+      return {x};
+    }
   };
 </script>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,28 +1,51 @@
 <template>
   <div class="lemon-tabs">
     <div class="lemon-tabs-nav">
-      <div class="lemon-tabs-nav-item" v-for="(t, index) in titles" :key="index">{{ t }}</div>
+      <div
+        class="lemon-tabs-nav-item"
+        :class="{selected: selected === p.key}"
+        v-for="p in tabProps" :key="p.key"
+        @click="select(p.key)"
+      >
+        {{ p.title }}
+      </div>
     </div>
     <div class="lemon-tabs-content">
-      <component class="lemon-tabs-content-item" v-for="(c, index) in defaults" :is="c" :key="index"/>
+      <component class="lemon-tabs-content-item" :is="current" :key="current.props.key"/>
     </div>
   </div>
 </template>
 <script lang="ts">
   import Tab from './Tab.vue';
+  import {computed} from 'vue';
 
   export default {
+    props: {
+      selected: {
+        type: String,
+      }
+    },
     setup(props, context) {
       const defaults = context.slots.default();
       defaults.forEach((tag) => {
         if (tag.type !== Tab) {
           throw new Error('Tabs 子标签必须是 Tab');
         }
+        if (tag.props.key === '') {
+          throw new Error('Tab 缺少 key');
+        }
       });
-      const titles = defaults.map((tag) => {
-        return tag.props.title;
+      const current = computed(() => {
+        return defaults.filter(tag => tag.props.key === props.selected)[0];
       });
-      return {defaults, titles};
+      const tabProps: { key: string, title: string } = defaults.map((tag) => {
+        console.log(tag.props);
+        return tag.props;
+      });
+      const select = (key: string) => {
+        context.emit('update:selected', key);
+      };
+      return {defaults, tabProps, current, select};
     }
   };
 </script>
