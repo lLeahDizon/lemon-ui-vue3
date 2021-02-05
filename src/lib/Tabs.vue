94b32438752c76de2963a6d3f147590c74e3cfd3
Lemon <516330797@qq.com>
2021-02-05 10:38:23 +0800

    tabs css 1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
-    <div v-for="(t, index) in titles" :key="index">{{ t }}</div>
-    <component v-for="(c, index) in defaults" :is="c" :key="index"/>
+  <div class="lemon-tabs">
+    <div class="lemon-tabs-nav">
+      <div class="lemon-tabs-nav-item" v-for="(t, index) in titles" :key="index">{{ t }}</div>
+    </div>
+    <div class="lemon-tabs-content">
+      <component class="lemon-tabs-content-item" v-for="(c, index) in defaults" :is="c" :key="index"/>
+    </div>
   </div>
 </template>
 <script lang="ts">
@@ -23,5 +27,32 @@
   };
 </script>
 <style lang="scss">
+  $blue: #40a9ff;
+  $color: #333;
+  $border-color: #d9d9d9;
+  .lemon-tabs {
+    &-nav {
+      display: flex;
+      color: $color;
+      border-bottom: 1px solid $border-color;
 
+      &-item {
+        padding: 8px 0;
+        margin: 0 16px;
+        cursor: pointer;
+
+        &:first-child {
+          margin-left: 0;
+        }
+
+        &.selected {
+          color: $blue;
+        }
+      }
+    }
+
+    &-content {
+      padding: 8px 0;
+    }
+  }
 </style>
