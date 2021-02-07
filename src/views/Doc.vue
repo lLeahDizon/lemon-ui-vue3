cbcef991a14b61dff77ab887cb716874194b9148
Lemon <516330797@qq.com>
2021-02-08 00:04:15 +0800

    复杂一点的高亮

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -50,10 +50,6 @@
   };
 </script>
 <style lang="scss" scoped>
-  .router-link-active {
-    text-decoration: underline;
-  }
-
   .layout {
     display: flex;
     flex-direction: column;
@@ -61,6 +57,8 @@
 
     > .nav {
       flex-shrink: 0;
+      background: white;
+      box-shadow: 0 2px 8px #f0f1f2;
     }
 
     > .content {
@@ -90,20 +88,28 @@
   aside {
     background: lightblue;
     width: 150px;
-    padding: 16px;
     position: fixed;
     top: 0;
     left: 0;
-    padding-top: 70px;
+    padding: 70px 0 16px;
     height: 100%;
 
     > h2 {
       margin-bottom: 4px;
+      padding: 0 16px;
     }
 
     > ol {
       > li {
-        padding: 4px 0;
+        > a {
+          display: block;
+          padding: 4px 16px;
+          text-decoration: none;
+        }
+
+        .router-link-active {
+          background: white;
+        }
       }
     }
   }
