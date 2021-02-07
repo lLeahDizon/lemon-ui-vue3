1902faa51581eed178d6b9256960608bd857b8d0
Lemon <516330797@qq.com>
2021-02-08 00:23:49 +0800

    填充文档内容

--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,12 +1,13 @@
 <template>
-  <div>
-    安装
-  </div>
+  <article>
+    <h1>安装</h1>
+    <p>打开终端运行下列命令：</p>
+    <pre><code>npm install lemon-ui</code></pre>
+    <p>或</p>
+    <pre><code>yarn add lemon-ui</code></pre>
+
+    <p>
+      下一节：<a href="#/doc/get-started">开始使用</a>
+    </p>
+  </article>
 </template>
-
-<script lang="ts">
-</script>
-
-<style lang="scss" scoped>
-
-</style>
--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,12 +1,7 @@
 <template>
-  <div>
-    Intro
-  </div>
+  <article>
+    <h1>介绍</h1>
+    <p>Lemon UI 是一款基于 Vue 3 和 TypeScript 的 UI 组件库。</p>
+    下一节：<a href="#/doc/install">安装</a>
+  </article>
 </template>
-
-<script lang="ts">
-</script>
-
-<style lang="scss" scoped>
-
-</style>
