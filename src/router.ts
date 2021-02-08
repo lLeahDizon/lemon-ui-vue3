14b6bfd30eb3614475ecd62a506915a5fd25fe5a
Lemon <516330797@qq.com>
2021-02-08 16:47:07 +0800

    优化 Markdown

--- src/router.ts
+++ src/router.ts
@@ -5,23 +5,22 @@ import SwitchDemo from './components/SwitchDemo.vue';
 import ButtonDemo from './components/ButtonDemo.vue';
 import DialogDemo from './components/DialogDemo.vue';
 import TabsDemo from './components/TabsDemo.vue';
-import DocDemo from './components/DocDemo.vue';
 
-import Intro from './views/Intro.vue';
-import GetStarted from './views/GetStarted.vue';
-import Install from './views/Install.vue';
+import {h} from 'vue';
+import Markdown from './components/Markdown.vue';
 
 const history = createWebHashHistory();
+const md = filename => h(Markdown, {path: `../markdown/${filename}.md`, key: filename});
 export const router = createRouter({
   history,
   routes: [
     {path: '/', component: Home},
     {
       path: '/doc', component: Doc, children: [
-        {path: '', component: DocDemo},
-        {path: 'intro', component: Intro},
-        {path: 'get-started', component: GetStarted},
-        {path: 'install', component: Install},
+        {path: '', redirect: '/doc/intro'},
+        {path: 'intro', component: md('intro')},
+        {path: 'get-started', component: md('getStarted')},
+        {path: 'install', component: md('install')},
         {path: 'switch', component: SwitchDemo},
         {path: 'button', component: ButtonDemo},
         {path: 'dialog', component: DialogDemo},
