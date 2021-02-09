9c24ef8387eb6c6b58fbe012200ad42fc82013bb
Lemon <516330797@qq.com>
2021-02-09 11:09:15 +0800

    改造 Button Demo

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,68 +1,33 @@
 <template>
-  <div>Button 示例</div>
-  <h1>示例1</h1>
   <div>
-    <Button>default</Button>
-    <Button theme="button">button</Button>
-    <Button theme="link">link</Button>
-    <Button theme="text">text</Button>
-  </div>
-  <h1>示例2</h1>
-  <div>
-    <Button size="big">Big Button</Button>
-    <Button>Normal Button</Button>
-    <Button size="small">Small Button</Button>
-  </div>
-  <div>
-    <Button theme="link" size="big">Big Button</Button>
-    <Button theme="link">Normal Button</Button>
-    <Button theme="link" size="small">Small Button</Button>
-  </div>
-  <div>
-    <Button theme="text" size="big">Big Button</Button>
-    <Button theme="text">Normal Button</Button>
-    <Button theme="text" size="small">Small Button</Button>
-  </div>
-  <h1>示例3</h1>
-  <div>
-    <dir>
-      <Button level="main">main</Button>
-      <Button>default</Button>
-      <Button level="danger">danger</Button>
-    </dir>
-    <dir>
-      <Button theme="link" level="main">main</Button>
-      <Button theme="link">default</Button>
-      <Button theme="link" level="danger">danger</Button>
-    </dir>
-    <dir>
-      <Button theme="text" level="main">main</Button>
-      <Button theme="text">default</Button>
-      <Button theme="text" level="danger">danger</Button>
-    </dir>
-  </div>
-  <h1>示例4</h1>
-  <div>
-    <Button disabled>disabled</Button>
-    <Button theme="link" disabled>disabled link</Button>
-    <Button theme="text" disabled>disabled text</Button>
-  </div>
-  <h1>示例5</h1>
-  <div style="display: flex;">
-    <Button loading>loading</Button>
-    <Button>loading complete</Button>
+    <div>Button 示例</div>
+    <Demo :component="Button1Demo"/>
+    <Demo :component="Button2Demo"/>
+    <Demo :component="Button3Demo"/>
+    <Demo :component="Button4Demo"/>
+    <Demo :component="Button5Demo"/>
   </div>
 </template>
 <script lang="ts">
-  import Button from '../lib/Button.vue';
+  import Demo from './Demo.vue';
+  import Button1Demo from './Button1.demo.vue';
+  import Button2Demo from './Button2.demo.vue';
+  import Button3Demo from './Button3.demo.vue';
+  import Button4Demo from './Button4.demo.vue';
+  import Button5Demo from './Button5.demo.vue';
 
   export default {
-    components: {Button},
+    components: {
+      Demo
+    },
     setup() {
-      const onClick = () => {
-        console.log('hi');
+      return {
+        Button1Demo,
+        Button2Demo,
+        Button3Demo,
+        Button4Demo,
+        Button5Demo
       };
-      return {onClick};
-    }
+    },
   };
 </script>
