45b3dbbe93727d72b0b746a3fc672f41cc9b6a5b
Lemon <516330797@qq.com>
2021-02-12 23:04:26 +0800

    使用 <script setup> 新特性

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,16 @@
   <router-view/>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
   import {provide, ref} from 'vue';
   import {router} from './router';
 
-  export default {
-    name: 'App',
-    setup() {
-      const width = document.documentElement.clientWidth;
-      const collapsed = ref(width > 500);
-      provide('collapsed', collapsed); // set
-      router.afterEach(() => {
-        if (width <= 500) {
-          collapsed.value = false;
-        }
-      });
+  const width = document.documentElement.clientWidth;
+  const collapsed = ref(width > 500);
+  provide('collapsed', collapsed); // set
+  router.afterEach(() => {
+    if (width <= 500) {
+      collapsed.value = false;
     }
-  };
+  });
 </script>
