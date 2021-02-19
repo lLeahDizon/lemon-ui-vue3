79859208e4c7ac0e6573e8b0de7c7e11c6aaab08
Lemon <516330797@qq.com>
2021-02-19 11:57:30 +0800

    更改部署到码云

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git init &&
 git add . &&
 git commit -m "update" &&
 git branch -M main &&
-git remote add origin git@github.com:lLeahDizon/lemon-ui-vue3-website.git &&
+git remote add origin git@gitee.com:LeahDizon/lemon-ui-vue3-website.git &&
 git push -f -u origin main &&
 cd -
-echo https://lleahdizon.github.io/lemon-ui-vue3-website/index.html
+echo https://leahdizon.gitee.io/lemon-ui-vue3-website/#/
